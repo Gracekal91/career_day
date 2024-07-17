ab1d47c4c4082f0f2e1fab116bdc883da23c1fc3
Grace <grace.kalombo@mondiadigital.com>
2024-07-17 14:01:02 +0200

    initial setup

---
+++ GirlCodeSession/guess_the_animal/final/script.js
@@ -0,0 +1,55 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const description = document.getElementById("description");
+    const animalImage = document.getElementById("animalImage");
+    const result = document.getElementById("result");
+    const tryAgainButton = document.getElementById("tryAgainButton");
+    const animalSelect = document.getElementById("animal");
+    const correctSound = document.getElementById("correctSound");
+    const container = document.getElementById("result-container");
+
+    let listOfAnimals = [
+        { id: 1, name: 'lion', description: 'I am the king of the jungle and I have a majestic mane.', imageLink: 'https://pngimg.com/uploads/lion/lion_PNG23271.png', sound: './assets/70333__mrbubble110__bear-roar.wav', color: '#DECC9C' },
+        { id: 2, name: 'elephant', description: 'I have a long trunk and large ears.', imageLink: 'https://png.pngtree.com/png-clipart/20230411/original/pngtree-elephant-asian-elephant-png-image_9043910.png', sound: './assets/139052__jasher70__elephant-scream.wav', color: '#9D9C8E' },
+        { id: 3, name: 'giraffe', description: 'I have a very long neck and I am the tallest land animal.', imageLink: 'https://png.pngtree.com/png-vector/20230918/ourmid/pngtree-giraffe-animal-isolated-3d-rendering-png-image_10089289.png', sound: './assets/70333__mrbubble110__bear-roar.wav', color: '#DECC9C' },
+        { id: 4, name: 'tiger', description: 'I have stripes and I am known for my strength and agility.', imageLink: 'https://pngimg.com/uploads/tiger/tiger_PNG23248.png', sound: './assets/571288__lewisbm__tiger_roar.wav', color: '#DECC9C' },
+        { id: 5, name: 'zebra', description: 'I have black and white stripes and I am related to horses.', imageLink: 'https://pngimg.com/uploads/zebra/zebra_PNG11209.png', sound: './assets/70333__mrbubble110__bear-roar.wav', color: '#DECC9C' },
+        { id: 6, name: 'monkey', description: 'I am known for my agility and intelligence, and I love bananas.', imageLink: 'https://png.pngtree.com/png-clipart/20230528/ourmid/pngtree-zebra-isolated-on-transparent-background-png-image_7110004.png', sound: './assets/588623__wilika2__monkey-noise-imitation.wav', color: '#DECC9C' },
+        { id: 7, name: 'bear', description: 'I am large and furry, and I love honey.', imageLink: 'https://png.pngtree.com/png-vector/20230318/ourmid/pngtree-brown-bear-animal-transparent-on-white-png-image_6655701.png', sound: './assets/70333__mrbubble110__bear-roar.wav', color: '#DECC9C' }
+    ];
+
+    let currentAnimal;
+
+    function getRandomAnimal() {
+        const randomIndex = Math.floor(Math.random() * listOfAnimals.length);
+        currentAnimal = listOfAnimals[randomIndex];
+        description.textContent = currentAnimal.description;
+    }
+
+    window.checkAnswer = function() {
+        const selectedAnimal = animalSelect.value;
+
+        if (selectedAnimal === currentAnimal.name) {
+            result.textContent = `Correct! It's a ${currentAnimal.name}.`;
+            animalImage.src = currentAnimal.imageLink;
+            animalImage.style.display = "block";
+            correctSound.src = currentAnimal.sound;
+            correctSound.play();
+            tryAgainButton.style.display = "none";
+            container.style.backgroundColor = currentAnimal.color;
+        } else {
+            result.textContent = "Incorrect. Try again!";
+            animalImage.style.display = "none";
+            tryAgainButton.style.display = "block";
+        }
+    };
+
+    window.tryAgain = function() {
+        result.textContent = "";
+        animalImage.style.display = "none";
+        tryAgainButton.style.display = "none";
+        animalSelect.value = "select";
+        getRandomAnimal();
+    };
+
+    getRandomAnimal();
+});
---
+++ GirlCodeSession/guess_the_number/final/script.js
@@ -0,0 +1,37 @@
+let randomNumber = Math.floor(Math.random() * 10) + 1;
+let guessCount = 0;
+
+document.getElementById("guess-button").addEventListener("click", () => {
+    const userGuess = Number(document.getElementById("guess-input").value);
+    const message = document.getElementById("message");
+    guessCount++;
+    if (userGuess === randomNumber) {
+        document.getElementById("image").style.display='block'
+        message.textContent = `Congratulations! You guessed the number in ${guessCount} attempts.`;
+        message.style.color = "green";
+        endGame();
+    } else if (userGuess < randomNumber) {
+        message.textContent = "Too low! Try again.";
+        message.style.color = "red";
+    } else if (userGuess > randomNumber) {
+        message.textContent = "Too high! Try again.";
+        message.style.color = "red";
+    }
+});
+
+function endGame() {
+    document.getElementById("guess-input").disabled = true;
+    document.getElementById("guess-button").style.display = "none";
+    document.getElementById("reset-button").style.display = "inline-block";
+}
+
+document.getElementById("reset-button").addEventListener("click", () => {
+    randomNumber = Math.floor(Math.random() * 10) + 1;
+    guessCount = 0;
+    document.getElementById("guess-input").value = "";
+    document.getElementById("guess-input").disabled = false;
+    document.getElementById("guess-button").style.display = "inline-block";
+    document.getElementById("reset-button").style.display = "none";
+    document.getElementById("image").style.display='none';
+    document.getElementById("message").textContent = "";
+});
